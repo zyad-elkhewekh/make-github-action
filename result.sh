97a29a9c5867a4b0c3672572d24febc1c3ad2162
zyad-elkhewekh <zyadsalah16@gmail.com>
2026-09-11 18:56:42 +0300

    distributed actions

--- result.sh
+++ result.sh
@@ -6,9 +6,7 @@ answer=$(cat "$SOLUTION")
 grade=$(./grade-sol.sh)
 
 if [[ "$grade" == "CONGRATS" ]]; then
-	touch "$FILE_NAME"
-	echo "Your solution: $answer , expected solution: 'special_devops!', verdict: PASS" >> "$FILE_NAME"
+	echo "Your solution: $answer , expected solution: 'special_devops!', verdict: PASS" > "$FILE_NAME"
 else
-	touch "$FILE_NAME"
-        echo "Your solution: $answer , expected solution: 'special_devops!', verdict: FAIL" >> "$FILE_NAME"
+        echo "Your solution: $answer , expected solution: 'special_devops!', verdict: FAIL" > "$FILE_NAME"
 fi
